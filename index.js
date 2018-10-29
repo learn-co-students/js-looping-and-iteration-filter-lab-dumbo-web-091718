af53de1866cf536c5822c2152fb26998ed3e4e6f
github username <github email address>
2018-10-28 23:21:36 -0400

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function fuzzyMatch(drivers, name) {
   return drivers.filter((driver) =>  driver.startsWith(name));
 }
  // Accesses the data structure to check if name matches
-function matchName (drivers, name) {
+function matchName(drivers, name) {
   return drivers.filter((driver) => {
     return driver.name.toLowerCase() === name.toLowerCase();
   });
@@ -27,25 +27,6 @@ function matchName (drivers, name) {
 //     at Context.<anonymous> (test/indexTest.js:43:7)
 // matchName()
 // accesses the data structure to check if name matches
-//
-// describe('findMatching()', function () {
-//   it('returns all drivers that match the passed in name', function () {
-//     const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-//
-//     expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'Bobby']);
-//     expect(findMatching(drivers, 'Sammy')).to.eql(['Sammy']);
-//   });
-//
-//   it('returns matching drivers if case does not match but letters do', function () {
-//     const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
-//
-//     expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'bobby']);
-//   });
-//
-//   it('returns an empty array if there is no match', function () {
-//     const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
-//
-//     expect(findMatching(drivers, 'Susan')).to.eql([]);
-//   });
-// });
+ // });
+ 
 // https://github.com/learn-co-students/js-looping-and-iteration-filter-lab-dumbo-web-091718
